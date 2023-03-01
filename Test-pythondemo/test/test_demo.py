80a1f637b1730821689960ec80c7029a817b1565
qingxuan_chen <qingxuan_chen@126.com>
2023-03-01 09:57:50 +0800

    python序列： 列表 元组 字典 集合

---
+++ Test-pythondemo/test/test_demo.py
@@ -0,0 +1,47 @@
+import random
+# sum1 = 0
+# for i in range(0, 101, 2):
+#     sum1 = sum1 + i
+# print(sum1)
+
+# i = 0
+# while i <= 100:
+#     sum1 = sum1 + i
+#     i += 1
+# print(sum1)
+
+# c_num = random.randint(1, 10)
+# while True:
+#     p_num = int(input("请输入数字： "))
+#
+#     if p_num > c_num:
+#         print("太大了")
+#     elif p_num < c_num:
+#         print("太小了")
+#     else:
+#         print("猜对了,游戏结束")
+#         break
+
+# result = []
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         result.append(i**2)
+# print(result)
+
+# result = [i**2 for i in range(1, 11) if i % 2 == 0]
+# print(result)
+
+# tuple1 = (1, )
+# print(type(tuple1), tuple1)
+# tuple2 = tuple('testmessage')
+# print(tuple2)
+
+# dict1 = {k: v for k, v in [('name', 'chen'), ('age', 18)]}
+# print(dict1)
+#
+# dict2 = {'a': 1, 'b': 2, 'c': 3, 'd': 3}
+# print(dict2.items())
+# dict3 = {k: v **2 for k, v in dict2.items() if v >1}
+# print(dict3)
+# dict4 = {v: k for k, v in dict2.items()}
+# print(dict4)
